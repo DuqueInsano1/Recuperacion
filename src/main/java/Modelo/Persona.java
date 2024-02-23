555435a4efc3870dfd9b6bbb3be3270044f91637
Belial <belialwolf39@gmail.com>
2024-02-23 13:27:26 -0500

    Examen de Recuperacion

---
+++ src/main/java/Modelo/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.time.LocalDate;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author Belial
+ */
+public class Persona {
+    private String nombres;
+    private String apellidos;
+  
+
+    public Persona() {
+    }
+
+    public Persona(String nombres, String apellidos) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+
+    
+}
